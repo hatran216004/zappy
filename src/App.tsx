253874f0706794ca9e8171df17a17213b92b2a6d
hatran216004 <qiqisokiu.321@gmail.com>
2018-10-29 20:34:11 +0700

    ui posts page

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,3 @@ export default function App() {
     </ConfirmPopupProvider>
   );
 }
-/*
-  ✅ Real-time 1:1 Chat
-  ✅ Send/Receive Text Messages
-  ✅ Send Images, Videos, Audio, Files
-  ✅ Reply to Messages
-  ✅ Edit Messages
-  ✅ Recall Messages
-  ✅ Message Reactions (Emoji)
-  ✅ Typing Indicators
-  ✅ Read Receipts
-  ✅ Unread Count
-  ✅ Infinite Scroll for Message History
-  ✅ Message Timestamps
-  ✅ Online/Offline Status
-  ✅ Conversation List with Last Message Preview
-  ✅ Create Direct Conversation from Friends List
-  ✅ Real-time Updates for All Features
-*/
