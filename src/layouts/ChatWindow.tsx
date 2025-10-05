6469738be2001e57cdc86be12cb518697ca547f1
Hieu Nguyen <hieunguyenb6524@gmail.com>
2025-10-05 13:56:24 +0700

    chinh sua chatwindow

--- src/layouts/ChatWindow.tsx
+++ src/layouts/ChatWindow.tsx
@@ -2,6 +2,7 @@ import ChatFooter from "@/components/ChatWindow/ChatFooter";
 import ChatHeader from "@/components/ChatWindow/ChatHeader";
 import ChatMessages from "@/components/ChatWindow/ChatMessages";
 import ContactWindow from "./ContactWindow";
+import Sidebar from "./Sidebar";
 
 type ChatWindowProps = {
   currentTab: string;
@@ -13,17 +14,20 @@ export default function ChatWindow({
   contentContact,
 }: ChatWindowProps) {
   return (
-    <div className="flex-1 flex flex-col bg-white dark:bg-gray-800">
-      {/* Header */}
-      <ChatHeader currentTab={currentTab} contentContact={contentContact} />
+    <>
+      <div className="flex-1 flex flex-col bg-white dark:bg-gray-800">
+        {/* Header */}
+        <ChatHeader currentTab={currentTab} contentContact={contentContact} />
 
-      {/* Content */}
-      <div className="flex-1 overflow-y-auto scrollbar-custom px-4 pt-4 bg-gray-100 dark:bg-gray-900">
-        {currentTab === "ContactList" ? <ContactWindow /> : <ChatMessages />}
+        {/* Content */}
+        <div className="flex-1 overflow-y-auto scrollbar-custom px-4 pt-4 bg-gray-100 dark:bg-gray-900">
+          {currentTab === "ContactList" ? <ContactWindow /> : <ChatMessages />}
+        </div>
+
+        {/* Footer */}
+        {currentTab === "ContactList" ? null : <ChatFooter />}
       </div>
-
-      {/* Footer */}
-      {currentTab === "ContactList" ? null : <ChatFooter />}
-    </div>
+      {currentTab === "ContactList" ? null : <Sidebar />}
+    </>
   );
 }
